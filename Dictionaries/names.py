7de15ef9c208126fcfb14849a1ee5ce1501913f5
Ibhav Malviya <144520056+IbhavMalviya@users.noreply.github.com>
2024-09-08 15:38:58 +0530

    Add files via upload

---
+++ Dictionaries/names.py
@@ -0,0 +1,5 @@
+person={'firstname':"Ibhav",'lastname':"Sharma",'age':20,'city':'Delhi'}
+print(person['firstname'])
+print(person['lastname'])
+print(person['age'])
+print(person['city'])
---
+++ Dictionaries/rivers.py
@@ -0,0 +1,7 @@
+rivers= {'yamuna':'india','nile':'egypt','mississippi':'usa'}
+for river in rivers:
+    print(f"{river} is a prominent river in the world")
+
+for country in rivers.values():
+    print(f"{country} is a country where the river flows")
+
