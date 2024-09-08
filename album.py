2d13b8f40bfaed1ef4fbaf031fc0ebb8c87450f8
Ibhav Malviya <144520056+IbhavMalviya@users.noreply.github.com>
2024-09-08 15:34:28 +0530

    Delete album.py

--- album.py
+++
@@ -1,9 +0,0 @@
-def make_album(artistname, albumtitle):
-    artistname=input("Enter the name of the artist: ")
-    albumtitle=input("Enter the title of the album: ")
-    album={'artist': artistname, 'title': albumtitle}
-    print(album)
-
-n=0
-while n<3:
-    make_album(None, None)
